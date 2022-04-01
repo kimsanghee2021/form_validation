7fd3951035053e8c8e46f716a6d98d9a70db0db7
sangheekim <tkdgmltjddnr@naver.com>
2022-04-01 11:22:13 +0900

    아이디 입력 없을시 alert 창 노출

--- js/validation.js
+++ js/validation.js
@@ -2,19 +2,22 @@ const form = document.querySelector('#member');
 const btnsSubmit = form.querySelector('input[type="submit"]');
 
 btnsSubmit.addEventListener('click',(e)=>{
-    if(!isTxt('userid')) e.preventDefault();
+    if(!isTxt('#userid')) e.preventDefault();
+    
 });
+
 function isTxt(name){
-    const input = form.querySelector(`[name] = ${name}`);
-    const txt = input.ariaValue;
+    const input = form.querySelector(`${name}`);
+    const txt = input.value;
+    
     if (txt == '') {
         alert('아이디를 입력하세요');
         return false;
     } else {
-        if (txt.length < 5) {
-            alert('아이디를 5글자이상 이상입력하세요')
+        if(txt.length < 5){
+            alert('5글자 이상 입력해주세요');
             return false;
-        } else {
+        } else{
             return true;
         }
     }
