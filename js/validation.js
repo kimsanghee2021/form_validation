3b85ee39145c1879e1b87996aed005b311bcafaf
kimsanghee <tkdgmltjddnr@naver.com>
2022-04-02 09:34:13 +0900

    input 텍스트 append massage 추가

--- js/validation.js
+++ js/validation.js
@@ -2,23 +2,23 @@ const form = document.querySelector('#member');
 const btnsSubmit = form.querySelector('input[type="submit"]');
 
 btnsSubmit.addEventListener('click',(e)=>{
-    if(!isTxt('#userid')) e.preventDefault();
-    
+    if(!isTxt('userid')) e.preventDefault();
 });
 
+//미션 1 - 아이디가 5글자 이상일떄 인증 통과 
+//인증 실패시 경고창이 아닌 input 옆에 '아이디를 입력하세요' 메세지 출력
 function isTxt(name){
-    const input = form.querySelector(`${name}`);
+    const input = form.querySelector(`[name = ${name}]`);
     const txt = input.value;
-    
-    if (txt == '') {
-        alert('아이디를 입력하세요');
+    const errMsg = document.createElement('p');
+    const errTxt = input.getAttribute('placeholder');
+
+    if(txt.length > 5){
+        return true;
+    }else{
+        errMsg.innerHTML = errTxt;
+        input.closest('td').append(errMsg);
         return false;
-    } else {
-        if(txt.length < 5){
-            alert('5글자 이상 입력해주세요');
-            return false;
-        } else{
-            return true;
-        }
     }
+
 }
