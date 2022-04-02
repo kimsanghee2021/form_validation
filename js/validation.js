b67d2e98f1213c3e3c9edd0896ef57097ef3f046
kimsanghee <tkdgmltjddnr@naver.com>
2022-04-02 10:18:17 +0900

    이메일 정규식 작업

--- js/validation.js
+++ js/validation.js
@@ -1,27 +1,48 @@
 const form = document.querySelector('#member');
-const btnsSubmit = form.querySelector('input[type="submit"]');
+const btnSubmit = form.querySelector('input[type=submit]');
 
-btnsSubmit.addEventListener('click',(e)=>{
-    if(!isTxt('userid')) e.preventDefault();
+btnSubmit.addEventListener('click', (e) => {
+	//if (!isTxt('userid', 5)) e.preventDefault();
+	if (!isEmail('email', 7)) e.preventDefault();
 });
 
-function isTxt(name){
-    const input = form.querySelector(`[name = ${name}]`);
-    const txt = input.value;
-    const errMsg = document.createElement('p');
-    errMsg.style.color = 'red';
-    const errTxt = input.getAttribute('placeholder');
-    const errMsgs = input.closest('td').querySelectorAll('p');
+function isTxt(name, len) {
+	const input = form.querySelector(`[name=${name}]`);
+	const txt = input.value;
 
+	if (txt.length > len) {
+		const errMsgs = input.closest('td').querySelectorAll('p');
+		if (errMsgs.length > 0) input.closest('td').querySelector('p').remove();
 
-    if(txt.legnth > 5){
-        if(errMsgs.length > 0)input.closest('td').querySelector('p').remove();
-        return true;
-    } else{
-        if(errMsgs.length > 0)input.closest('td').querySelector('p').remove();
-        errMsg.innerHTML = errTxt;
-        input.closest('td').append(errMsg);
-        return false;
-    }
+		return true;
+	} else {
+		const errMsgs = input.closest('td').querySelectorAll('p');
+		if (errMsgs.length > 0) input.closest('td').querySelector('p').remove();
 
+		const errMsg = document.createElement('p');
+        errMsg.innerText = `아이디값을 ${len}이상 입력하세요.`
+		input.closest('td').append(errMsg);
+		return false;
+	}
+}
+
+function isEmail(name, len) {
+	const input = form.querySelector(`[name=${name}]`);
+	const txt = input.value;
+
+	if (txt.length > len && /@/.test(txt)) {
+		const errMsgs = input.closest('td').querySelectorAll('p');
+		if (errMsgs.length > 0) input.closest('td').querySelector('p').remove();
+
+		return true;
+	} else {
+		const errMsgs = input.closest('td').querySelectorAll('p');
+		if (errMsgs.length > 0) input.closest('td').querySelector('p').remove();
+
+		const errMsg = document.createElement('p');
+		const errTxt = input.getAttribute('placeholder');
+		errMsg.innerText = errTxt;
+		input.closest('td').append(errMsg);
+		return false;
+	}
 }
