f3b71db9233c7ff25834d1f8eea1342f5f7e2302
sangheekim <tkdgmltjddnr@naver.com>
2022-04-05 14:42:06 +0900

    select 박스 함수 작성 완료

--- js/validation.js
+++ js/validation.js
@@ -4,20 +4,17 @@ const btnSubmit = document.querySelector('input[type="submit"]');
 btnSubmit.addEventListener('click',(e)=>{
 	if(!isTxt('userid',5)) e.preventDefault();
 	if(!isTxt('email')) e.preventDefault();
+	if(!isSelect('edu')) e.preventDefault();
 });
 
 function isTxt(target,len){
 	const inputName = form.querySelector(`[name = ${target}]`);
 	const val = inputName.value;
 
-	// 아이디가 5글자 이상일때 인증통과
-	//인증 실패시 경고창이 아닌 input옆에 '아이디를 입력하세요'라는 메세지 출력
 	if(val.length > len){
-
 		//에러메세지를 처음에 다 초기화 시키기
 		const errMsgs = inputName.closest('td').querySelectorAll('p');
 		if(errMsgs.length > 0) inputName.closest('td').querySelector('p').remove();
-
 		return true;
 	} else{
 
@@ -52,11 +49,32 @@ function isEmail(target){
 
 		//에러메세지 생성
 		const errMsg = document.createElement('p');
-		const errtit = inputName.getAttribute('placeholder');
+		const errtit = '이메일 항목은 @포함해서 쓰세요';
 		errMsg.innerText = errtit;
 		inputName.closest('td').append(errMsg);
 	}
 }
-function isSelct(target,len){
 
+function isSelect(target){
+	const sel = form.querySelector(`[name = ${target}]`);
+	const sel_idx = sel.options.selectedIndex;
+	const sel_val = sel.options[sel_idx].value;
+
+	//selet박스에 선택되어있다면
+	if(sel_val != ''){
+		const errMsgs = sel.closest('td').querySelectorAll('p');
+		if(errMsgs.length > 0) sel.closest('td').querySelector('p').remove();
+
+		return true;
+
+	} else{
+		const errMsgs = sel.closest('td').querySelectorAll('p');
+		if(errMsgs.length > 0) sel.closest('td').querySelector('p').remove();
+
+		const errMsg = document.createElement('p');
+		const errtit = '학력을 선택해주세요.';
+		errMsg.innerText = errtit;
+		sel.closest('td').append(errMsg);
+	}
 }
+
