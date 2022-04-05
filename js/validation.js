561646bdd145865ef914ba80f2eab0c443fabe48
sangheekim <tkdgmltjddnr@naver.com>
2022-04-05 11:23:39 +0900

    아이디 에러메세지 생성

--- js/validation.js
+++ js/validation.js
@@ -1,26 +1,30 @@
-
 const form = document.querySelector('#member');
 const btnSubmit = document.querySelector('input[type="submit"]');
 
-btnSubmit.addEventListener('click',function(e){
-	if(!isid('userid')) e.preventDefault();
-	
+btnSubmit.addEventListener('click',(e)=>{
+	if(!isTxt('userid',5)) e.preventDefault();
 });
 
-//아이디가 5글자 이하일 떄 전송 중지하고 하단에 에러메세지 출력 
-function isid(target){
-	const input = form.querySelector(`[name = ${target}]`);
-	const txt = input.value;
-	if(txt.length > 5){
+function isTxt(target,len){
+	const inputName = form.querySelector(`[name = ${target}]`);
+	const val = inputName.value;
+	
+	// 아이디가 5글자 이상일때 인증통과
+	//인증 실패시 경고창이 아닌 input옆에 '아이디를 입력하세요'라는 메세지 출력
+	if(val.length > len){
 		return true;
-	}else{
+	} else{
+
+		//에러메세지를 처음에 다 초기화 시키기
+		const errMsgs = inputName.closest('td').querySelectorAll('p');
+		if(errMsgs.length > 0) inputName.closest('td').querySelector('p').remove();
+
+		//에러메세지 생성
 		const errMsg = document.createElement('p');
-		const errTxt = input.getAttribute('placeholder'); //아이디를 입력하세요
-		errMsg.innerText = errTxt;
-		input.closest('td').append(errMsg);
-		//alert(errTxt);
+		const errtit = inputName.getAttribute('placeholder');
+		errMsg.innerText = errtit;
+		inputName.closest('td').append(errMsg);
+		
 		return false;
 	}
 }
-
-
