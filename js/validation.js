0d758f4fd84a048f2d675e4331fe0eff34e1a091
sangheekim <tkdgmltjddnr@naver.com>
2022-04-05 14:18:14 +0900

    select 함수 제작

--- js/validation.js
+++ js/validation.js
@@ -3,15 +3,21 @@ const btnSubmit = document.querySelector('input[type="submit"]');
 
 btnSubmit.addEventListener('click',(e)=>{
 	if(!isTxt('userid',5)) e.preventDefault();
+	if(!isTxt('email')) e.preventDefault();
 });
 
 function isTxt(target,len){
 	const inputName = form.querySelector(`[name = ${target}]`);
 	const val = inputName.value;
-	
+
 	// 아이디가 5글자 이상일때 인증통과
 	//인증 실패시 경고창이 아닌 input옆에 '아이디를 입력하세요'라는 메세지 출력
 	if(val.length > len){
+
+		//에러메세지를 처음에 다 초기화 시키기
+		const errMsgs = inputName.closest('td').querySelectorAll('p');
+		if(errMsgs.length > 0) inputName.closest('td').querySelector('p').remove();
+
 		return true;
 	} else{
 
@@ -27,4 +33,30 @@ function isTxt(target,len){
 		
 		return false;
 	}
+}
+function isEmail(target){
+	const inputName = form.querySelector(`[name = ${target}]`);
+	const val = inputName.value;
+	const emailRule = /^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*.[a-zA-Z]{2,3}$/i;//이메일 정규식
+
+	if(val.match(emailRule)!=null){
+		const errMsgs = inputName.closest('td').querySelectorAll('p');
+		if(errMsgs.length > 0) inputName.closest('td').querySelector('p').remove();
+		return true;
+
+	} else{
+		
+		//에러메세지를 처음에 다 초기화 시키기
+		const errMsgs = inputName.closest('td').querySelectorAll('p');
+		if(errMsgs.length > 0) inputName.closest('td').querySelector('p').remove();
+
+		//에러메세지 생성
+		const errMsg = document.createElement('p');
+		const errtit = inputName.getAttribute('placeholder');
+		errMsg.innerText = errtit;
+		inputName.closest('td').append(errMsg);
+	}
+}
+function isSelct(target,len){
+
 }
