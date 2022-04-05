3f95df47062f1f490748a2622592b58cfc9ef8e5
sangheekim <tkdgmltjddnr@naver.com>
2022-04-05 10:44:24 +0900

    아이디가 5글자 이하일 떄 전송 중지하고 하단에 에러메세지 출력

--- js/validation.js
+++ js/validation.js
@@ -1,97 +1,26 @@
-const form = document.querySelector('#member');
-const btnSubmit = form.querySelector('input[type=submit]');
 
-btnSubmit.addEventListener('click', (e) => {
-	if (!isTxt('userid', 5)) e.preventDefault();
-	if (!isEmail('email', 7)) e.preventDefault();
-	if (!isCheck('gender')) e.preventDefault();
-    if(!isTxt('comments',10)) e.preventDefault();
-    if(!isSelect('edu'))e.preventDefault();
+const form = document.querySelector('#member');
+const btnSubmit = document.querySelector('input[type="submit"]');
+
+btnSubmit.addEventListener('click',function(e){
+	if(!isid('userid')) e.preventDefault();
+	
 });
 
-function isTxt(name, len) {
-	const input = form.querySelector(`[name=${name}]`);
+//아이디가 5글자 이하일 떄 전송 중지하고 하단에 에러메세지 출력 
+function isid(target){
+	const input = form.querySelector(`[name = ${target}]`);
 	const txt = input.value;
-
-	if (txt.length > len) {
-		const errMsgs = input.closest('td').querySelectorAll('p');
-		if (errMsgs.length > 0) input.closest('td').querySelector('p').remove();
-
+	if(txt.length > 5){
 		return true;
-	} else {
-		const errMsgs = input.closest('td').querySelectorAll('p');
-		if (errMsgs.length > 0) input.closest('td').querySelector('p').remove();
-
+	}else{
 		const errMsg = document.createElement('p');
-        errMsg.innerText = `해당항목을 ${len}이상 입력하세요.`
-		input.closest('td').append(errMsg);
-		return false;
-	}
-}
-
-function isEmail(name, len) {
-	const input = form.querySelector(`[name=${name}]`);
-	const txt = input.value;
-
-	if (txt.length > len && /@/.test(txt)) {
-		const errMsgs = input.closest('td').querySelectorAll('p');
-		if (errMsgs.length > 0) input.closest('td').querySelector('p').remove();
-
-		return true;
-
-	} else {
-		const errMsgs = input.closest('td').querySelectorAll('p');
-		if (errMsgs.length > 0) input.closest('td').querySelector('p').remove();
-
-		const errMsg = document.createElement('p');
-		const errTxt = input.getAttribute('placeholder');
+		const errTxt = input.getAttribute('placeholder'); //아이디를 입력하세요
 		errMsg.innerText = errTxt;
 		input.closest('td').append(errMsg);
+		//alert(errTxt);
 		return false;
 	}
 }
 
-function isCheck(name){
-    const inputs = form.querySelectorAll(`[name = ${name}]`);
-    let isChecked = false;
-    //모든 체크 인풋 요소를 반복돌며 하나라도 체크가 되어있으면 isChecked에 true 할당
-    for(const input of inputs) if(input.checked) isChecked =true;
 
-    //체크된 요소가 있을때
-    if(isChecked){
-        const errMsgs = inputs[0].closest('td').querySelectorAll('p');
-        if(errMsgs.length > 0) inputs[0].closest('td').querySelector('p').remove();
-        return true;
-    } else{
-        const errMsgs = inputs[0].closest('td').querySelectorAll('p');
-        if(errMsgs.length > 0) inputs[0].closest('td').querySelector('p').remove();
-
-        const errMsg = document.createElement('p');
-		errMsg.innerText = '필수 항목을 하나이상 체크해주세요';
-		inputs[0].closest('td').append(errMsg);
-
-		return false;
-    }
-}
-
-function isSelect(name){
-    const sel = document.querySelector(`[name = ${name}]`);
-    const sel_index = sel.options.selectedIndex;
-    const val = sel.options[sel_index].value;
-
-    //selector가 하나라도 잇을떄 
-    if(val!==''){
-        const errMsgs = sel[0].closest('td').querySelectorAll('p');
-        if(errMsgs.length > 0) sel[0].closest('td').querySelector('p').remove();
-        return true;
-    } else{
-        const errMsgs = sel[0].closest('td').querySelectorAll('p');
-        if(errMsgs.length > 0) sel[0].closest('td').querySelector('p').remove();
-
-        const errMsg = document.createElement('p');
-		errMsg.innerText = '최종학력을 체크해주세요';
-		sel.closest('td').append(errMsg);
-
-		return false;
-    }
-}
